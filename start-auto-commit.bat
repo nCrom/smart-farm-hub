385d021119962c475330f1817b322c4b95852ffa
nCrom <realpano@naver.com>
2025-02-15 21:06:17 +0900

    ?먮룞 而ㅻ컠: 2025-02-15 21:06:17

--- start-auto-commit.bat
+++ start-auto-commit.bat
@@ -1,9 +1,5 @@
 @echo off
 chcp 65001 > nul
-set LANG=ko_KR.UTF-8
-set LC_ALL=ko_KR.UTF-8
-set GIT_COMMITTER_ENCODING=utf-8
-set GIT_AUTHOR_ENCODING=utf-8
 set GIT_TERMINAL_PROMPT=0
-powershell -NoProfile -ExecutionPolicy Bypass -NoExit -Command "& {$PSDefaultParameterValues['*:Encoding'] = 'utf8'; [Console]::OutputEncoding = [System.Text.Encoding]::UTF8; & '%~dp0auto-commit.ps1'}"
+powershell -NoProfile -ExecutionPolicy Bypass -NoExit -Command "& {$OutputEncoding = [Console]::OutputEncoding = [System.Text.Encoding]::UTF8; & '%~dp0auto-commit.ps1'}"
 pause
